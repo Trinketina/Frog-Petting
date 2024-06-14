3071780d8703c2e0d8ce431fb041d770f56a0dce
Tina <143371814+Trinketina@users.noreply.github.com>
2024-06-13 23:25:02 -0500

    update to 1.21
    Updated to 1.21

--- src/main/java/net/trinketina/frogpetting/FrogPettingModClient.java
+++ src/main/java/net/trinketina/frogpetting/FrogPettingModClient.java
@@ -1,8 +1,6 @@
 package net.trinketina.frogpetting;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.ClickEvent;
