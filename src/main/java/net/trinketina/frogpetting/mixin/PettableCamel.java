cff1131fd0a6c11a4ae7d483029df5077c6d2e59
Tina <143371814+Trinketina@users.noreply.github.com>
2025-03-31 02:29:28 -0500

    Camel and Ocelot
    Camel flops ears when pet
       and has specific offset for both standing and sitting
    
    Ocelot purrs like a cat

--- src/main/java/net/trinketina/frogpetting/mixin/PettableCamel.java
+++ src/main/java/net/trinketina/frogpetting/mixin/PettableCamel.java
@@ -1,15 +1,13 @@
 package net.trinketina.frogpetting.mixin;
 
+import net.minecraft.entity.AnimationState;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.entity.passive.CamelEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import net.trinketina.frogpetting.FrogPettingMod;
-import net.trinketina.frogpetting.FrogPettingModClient;
 import net.trinketina.frogpetting.PettableInterface;
 import net.trinketina.frogpetting.config.PettingConfig;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,14 +17,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractHorseEntity.class)
-public abstract class PettableRideables extends PettingMixin implements PettableInterface {
+@Mixin(CamelEntity.class)
+abstract class PettableCamel extends PettingMixin implements PettableInterface {
     @Unique
-    protected double vertical_particle_offset = 1.5d;
+    protected double vertical_particle_offset = 2.5d;
+    @Unique
+    protected double vertical_sitting_particle_offset = .7d;
 
-    @Override public boolean uniqueRequirements(PlayerEntity player, Hand hand) {return player.isSneaking();}
-    //@Override public void uniqueInteraction(PlayerEntity player, Hand hand) {super.uniqueInteraction(player, hand);}
-
+    //@Override public boolean uniqueRequirements(PlayerEntity player, Hand hand) {return super.uniqueRequirements(player, hand);}
+    @Override public void uniqueInteraction(PlayerEntity player, Hand hand) {
+        idlingAnimationState.start(this.age - 50);
+        super.uniqueInteraction(player, hand);
+    }
+    @Override public double getVerticalOffset() {
+        if (isSitting())
+            return vertical_sitting_particle_offset;
+        return vertical_particle_offset;
+    }
+    //@Override public double getForwardOffset() {return horizontal_particle_offset;}
     @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
     public void onInteractMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (this.age > last_pet + PettingConfig.COOLDOWN*2 ) {
@@ -39,11 +47,9 @@ public abstract class PettableRideables extends PettingMixin implements Pettable
         return;
     }
 
-    @Override public double getVerticalOffset() {
-        return vertical_particle_offset;
-    }
-
-    protected PettableRideables(EntityType<? extends AnimalEntity> entityType, World world) {
+    @Shadow public boolean isSitting() {return false;}
+    @Shadow public final AnimationState idlingAnimationState = new AnimationState();
+    protected PettableCamel(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
 }
