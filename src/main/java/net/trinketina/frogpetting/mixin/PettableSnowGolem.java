300f4ea0692c48c31d71afc6fa331ff058797d84
Tina <143371814+Trinketina@users.noreply.github.com>
2025-03-31 02:52:55 -0500

    parrot and snow golem
    parrots are a bit finnicky if you want them on your shoulder but it works

--- src/main/java/net/trinketina/frogpetting/mixin/PettableSnowGolem.java
+++ src/main/java/net/trinketina/frogpetting/mixin/PettableSnowGolem.java
@@ -1,49 +1,47 @@
 package net.trinketina.frogpetting.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.Flutterer;
-import net.minecraft.entity.mob.Angerable;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.Shearable;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.entity.passive.PandaEntity;
+import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.trinketina.frogpetting.PettableInterface;
+import net.trinketina.frogpetting.config.PettingConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PandaEntity.class)
-public abstract class PettablePanda extends AnimalEntity implements PettableInterface {
+@Mixin(SnowGolemEntity.class)
+public abstract class PettableSnowGolem extends PettingMixin implements PettableInterface, Shearable {
     @Unique
-    protected double vertical_particle_offset = 1.5d;
+    protected double vertical_particle_offset = 1.9d;
+    @Unique
+    protected double vertical_sheared_particle_offset = 1.65d;
 
-    /*@Override public void uniqueInteraction(PlayerEntity player, Hand hand) {
-        this.getWorld().playSoundFromEntityClient(this, getAmbientSound(), SoundCategory.AMBIENT, this.getSoundVolume(), this.getSoundPitch());
-
-    }*/
     @Override public boolean uniqueRequirements(PlayerEntity player, Hand hand) {return !player.isSneaking();}
+    //@Override public void uniqueInteraction(PlayerEntity player, Hand hand) {super.uniqueInteraction(player, hand);}
     @Override public double getVerticalOffset() {
-        return vertical_particle_offset;
+        if (isShearable())
+            return vertical_particle_offset;
+        return vertical_sheared_particle_offset;
     }
+    //@Override public double getForwardOffset() {return horizontal_particle_offset;}
 
     @Inject(method = "interactMob", at = @At("RETURN"), cancellable = true)
     public void onInteractMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        if (cir.getReturnValue() == ActionResult.SUCCESS) return;
+        if (cir.getReturnValue() != ActionResult.PASS) return;
 
         super.interactMob(player, hand);
         cir.setReturnValue(ActionResult.SUCCESS);
+        return;
     }
 
-
-    protected PettablePanda(EntityType<? extends AnimalEntity> entityType, World world) {
+    protected PettableSnowGolem(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
 }
