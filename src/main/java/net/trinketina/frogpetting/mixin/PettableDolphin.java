6da6a67aa11dcadedf20a673db181c753ce316dd
Tina <143371814+Trinketina@users.noreply.github.com>
2025-09-28 19:57:00 -0500

    should fix #4
    left some interfaces in some animals that shouldn't have them, which caused null exceptions with mods that checked for them.

--- src/main/java/net/trinketina/frogpetting/mixin/PettableDolphin.java
+++ src/main/java/net/trinketina/frogpetting/mixin/PettableDolphin.java
@@ -12,10 +12,11 @@ import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.trinketina.frogpetting.PettableInterface;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(DolphinEntity.class)
-public abstract class PettableDolphin extends PettingMixin implements Angerable, Flutterer, PettableInterface {
+public abstract class PettableDolphin extends PettingMixin implements PettableInterface {
     @Unique
     protected double vertical_particle_offset = .5d;
 
