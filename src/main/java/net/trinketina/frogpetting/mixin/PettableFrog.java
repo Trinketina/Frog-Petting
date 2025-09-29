97acc4faae41d7108132d73c4d985673f140b57f
Tina <143371814+Trinketina@users.noreply.github.com>
2025-09-28 20:41:41 -0500

    backport fix 6da6a67aa11dcadedf20a673db181c753ce316dd

--- src/main/java/net/trinketina/frogpetting/mixin/PettableFrog.java
+++ src/main/java/net/trinketina/frogpetting/mixin/PettableFrog.java
@@ -17,9 +17,7 @@ import org.spongepowered.asm.mixin.Unique;
 
 
 @Mixin(FrogEntity.class)
-public abstract class PettableFrog
-    extends PettingMixin
-        implements PettableInterface {
+public abstract class PettableFrog extends PettingMixin implements PettableInterface {
     @Unique
     protected double vertical_particle_offset = .5d;
 
