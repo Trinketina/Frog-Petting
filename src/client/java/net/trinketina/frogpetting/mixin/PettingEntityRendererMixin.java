5957421ec1cd8fb62b7ba5acb9e2bb78dbfb1851
Tina <143371814+Trinketina@users.noreply.github.com>
2025-10-01 23:22:53 -0500

    added chicken animation

--- src/client/java/net/trinketina/frogpetting/mixin/PettingEntityRendererMixin.java
+++ src/client/java/net/trinketina/frogpetting/mixin/PettingEntityRendererMixin.java
@@ -1,17 +1,21 @@
 package net.trinketina.frogpetting.mixin;
 
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.trinketina.frogpetting.IPettingAnimationState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EntityRenderer.class)
-public abstract class PettingEntityRendererMixin<T extends Entity, S extends EntityRenderState> {
-    @Inject(method = "updateRenderState", at = @At("RETURN"))
+@Mixin(LivingEntityRenderer.class)
+public abstract class PettingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> {
+    @Inject(method = "updateRenderState*", at = @At("RETURN"))
     private void onUpdateRenderState(T livingEntity, S livingEntityRenderState, float f, CallbackInfo ci) {
         if (livingEntityRenderState instanceof IPettingAnimationState && livingEntity instanceof IPettingAnimationState) {
             IPettingAnimationState pettingRenderState = (IPettingAnimationState) livingEntityRenderState;
