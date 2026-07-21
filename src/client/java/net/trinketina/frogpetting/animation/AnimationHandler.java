Trinketina/Frog-Petting

commit b17e2b09c1fbfea08b8c333ec0ddbeed78f3ae06

minor bugfix
animation state kept running even after the animation finished.

could've theoretically caused lag? but more just to be safe than anything else

src/client/java/net/trinketina/frogpetting/animation/AnimationHandler.java
@@ -16,6 +16,10 @@ public class AnimationHandler {
         Vector3f scratch_vector = new Vector3f();
 
         animate(model, animation, running_time, 1.0f, scratch_vector);
+
+        if (current_time > animation.lengthS()) {
+            animationState.stop();
+        }
     }
 
     public static void animate(Model model, Animation animation, float running_time, float scale, Vector3f position) {

commit a21adfcd061d2bd8c825271dddb177d37ab926b7

proper fix for b17e2b09c1fbfea08b8c333ec0ddbeed78f3ae06
accidentally used the wrong time variable, leading to all animations failing to run. this is now fixed properly!

src/client/java/net/trinketina/frogpetting/animation/AnimationHandler.java
@@ -17,7 +17,7 @@ public class AnimationHandler {
 
         animate(model, animation, running_time, 1.0f, scratch_vector);
 
-        if (current_time > animation.lengthS()) {
+        if (running_time > animation.lengthS()) {
             animationState.stop();
         }
     }